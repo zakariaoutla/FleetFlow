62a001f1306f99219890134fbdd73572eb9aa7c7
zakaria outla <zakariadevlop78@gmail.com>
2026-04-01 12:21:27 +0100

    done

--- src/main/java/com/example/FleetFlow/mapper/VehilculeMapper.java
+++ src/main/java/com/example/FleetFlow/mapper/VehilculeMapper.java
@@ -2,9 +2,10 @@ package com.example.FleetFlow.mapper;
 
 import com.example.FleetFlow.dto.VehiculeDTO;
 import com.example.FleetFlow.Models.Vehicule;
+import org.mapstruct.Mapper;
 import org.springframework.web.bind.annotation.Mapping;
 
-@Mapping()
+@Mapper(componentModel = "spring")
 public interface VehilculeMapper {
 
     VehiculeDTO toDTO(Vehicule vehicule);
--- src/main/java/com/example/FleetFlow/repository/VehiculeRepository.java
+++ src/main/java/com/example/FleetFlow/repository/VehiculeRepository.java
@@ -1,7 +1,14 @@
 package com.example.FleetFlow.repository;
 
 import com.example.FleetFlow.Models.Vehicule;
+import com.example.FleetFlow.dto.VehiculeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VehiculeRepository extends JpaRepository<Vehicule, Long> {
+
+    List<VehiculeDTO> findByStatut(String status);
+
+    List<VehiculeDTO> findByCapaciteGreaterThan(int capacite);
 }
