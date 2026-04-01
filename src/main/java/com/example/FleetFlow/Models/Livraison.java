19b64a56c1e81bc30d33f496f26b07911b7e2e75
Marouane bounou <maroinbounou.123@gmail.com>
2026-04-01 17:46:08 +0100

    Create entities

--- src/main/java/com/example/FleetFlow/Models/Livraison.java
+++ src/main/java/com/example/FleetFlow/Models/Livraison.java
@@ -1,4 +1,35 @@
 package com.example.FleetFlow.Models;
 
+import com.example.FleetFlow.enums.StatutLivraison;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "livraisons")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class Livraison {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private LocalDateTime dateLivraison;
+    private String adresseDepart;
+    private String adressseDestination;
+
+    @Enumerated(EnumType.STRING)
+    private StatutLivraison statut;
+
+    @ManyToOne
+    @JoinColumn(name = "chauffeur_id")
+    private Chauffeur chauffeur;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicules_id")
+    private Vehicule vehicule;
+
+    @ManyToOne
+    @JoinColumn(name = "clients_id" , nullable = false)
+    private Client client;
 }
